d88b6ced003c3966b6e516e7431356552bd1b10e
Daniele <danielee.gamba@gmail.com>
2018-05-21 15:20:30 +0200

    appunti

--- generici/Pila.java
+++ generici/Pila.java
@@ -1,10 +1,34 @@
 package com.gft.esempi.generici;
 
-// implemento una pila di oggetti generici
+// implementa una pila di oggetti generici
+
 public class Pila<E> {
 	private final int dimensione;
 	private int topPila;
 	private E[] elementi;
 	
+	@SuppressWarnings("unchecked")
+	public Pila( int i ) {
+		// se i è positivo setto i, altrimenti 10
+		dimensione = i > 0 ? i : 10;
+		topPila = -i;
+		// faccio un casting rapido, per evitare controlli troppo lunghi
+		// e sopprimo il warning
+		elementi = (E[]) new Object[dimensione];
+		// infatti non posso creare un array di un tipo generico
+	}
+	
+	public void push( E valore ) {
+		if( topPila == dimensione - 1 ) // pila piena
+			throw new RuntimeException( "La pila è piena. Impossibile inserire" );
+		elementi[ ++topPila ] = valore;
+	}
+	
+	public E pop() {
+		if( topPila == -1 )
+			throw new RuntimeException( "La pila è vuota. Impossibile eliminare" );
+			// throw fa anche return
+		return elementi[ --topPila ];
+	}
 	
 }
---
+++ generici/PilaTest.java
@@ -0,0 +1,51 @@
+package com.gft.esempi.generici;
+
+public class PilaTest {
+	// occhio: qui devo usare i tipi Wrapper, non primitivi!!
+	// altrimenti non posso generalizzarli!!
+	// (i generici non possono essere primitivi!!)
+	private static Integer[] elementiInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+	private static Double[] elementiDouble = { 1.1, 2.2, 3.2, 4.2, 5.2 };
+	
+	private static Pila<Integer> iPila;
+	private static Pila<Double> dPila;
+	
+	public static void main(String[] args) {
+		
+		iPila = new Pila<Integer>(10);
+		dPila = new Pila<Double>(4);
+		
+		testPush( iPila, elementiInt );
+		testPush( dPila, elementiDouble );
+		//testPop( iPila, elementiInt );
+		//testPop( dPila, elementiDouble );
+		
+	}
+	
+	// per classi e interfacce si usa la T
+	// per metodi si usa la E
+	private static <T> void testPush( Pila<T> pila, T[] elementi ) {
+		try {
+			for( T elemento: elementi ) {
+				System.out.printf( "%s ", elemento );
+				pila.push( elemento );
+			}
+		} catch( RuntimeException exc ) {
+			exc.printStackTrace();
+		}
+	}
+	
+	private static <T> void testPop( Pila<T> pila, T[] elementi ) {
+		try {
+			 T popValue;
+			 while( true ) {
+				 popValue = pila.pop();
+				 System.out.printf( "%s" , popValue);
+			 }
+		} catch( RuntimeException exc ) {
+			System.out.println();
+			exc.printStackTrace();
+		}
+	}
+	
+}
